36687e7f8990f552ae21b34821a87e31c7e9b32c
Alisdair Templeton <alisdair73@gmail.com>
2017-02-14 00:59:47 +0000

    Initial Commit (local)

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("hnd.dpe.warranty.claim_inquirywarranty_claim_inquiry.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("hnd.dpe.warranty.claim_inquirywarranty_claim_inquiry.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
